ce803d689ebf25b2b43b94a38824118c8727549f
Michael Murray <Michael Murray>
2016-01-30 12:35:53 -0800

    Updating XAML layout

--- PetFinder/PetFinder/MainWindow.xaml.cs
+++ PetFinder/PetFinder/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using PetFinder.Core;
+﻿using System.Windows;
 using PetFinder.Core.Services;
 
 namespace PetFinder
@@ -27,7 +13,7 @@ namespace PetFinder
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void randomPetsButton_Click(object sender, RoutedEventArgs e)
         {
             // checking to see if MD5 Hash worked
             string pet = PetService.GetPet();
